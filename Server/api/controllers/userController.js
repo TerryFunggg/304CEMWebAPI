10b53e88b622a97787cd6506f45cb0b477eaa0fd
= <terryyessfung@gmail.com>
2020-03-31 23:02:00 +0800

    done the user function

--- Server/api/controllers/userController.js
+++ Server/api/controllers/userController.js
@@ -5,3 +5,96 @@
  * @since: Monday, 30th March 2020 10:27:29 pm
  * -----
  */
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
+const hashPwd = require("../middleware/hashpwd");
+const config = require("../../config");
+
+/**
+ *  Register new user function
+ *  It will using bcrypt to hash the user password,
+ *  After that insert new User in DB
+ *  @returns 201 Created status
+ *  @throws 409 - ConflictError
+ */
+exports.user_register = async (ctx, next) => {
+  let body = ctx.request.body;
+  const user = await User.find({ email: body.email });
+  if (user.length >= 1) {
+    // email already exit in DB
+    ctx.status = 409;
+    ctx.body = {
+      message: "email already exits",
+      code: -1
+    };
+  } else {
+    // create new user
+    const { email, name, password } = ctx.request.body;
+    const newUser = new User({ email, name, password });
+    const hash = await hashPwd(password);
+    newUser.password = hash;
+    try {
+      const result = await newUser.save();
+      ctx.status = 201;
+      ctx.body = {
+        message: "User Creted",
+        code: 0
+      };
+    } catch (err) {
+      ctx.status = 500;
+      ctx.body = {
+        message: "Can not create user.",
+        code: -1
+      };
+    }
+  }
+
+  //const user = new User({ email, name, password });
+
+  // // hash password to user
+  // bcrypt.genSalt(10, (err, salt) => {
+  // bcrypt.hash(user.password, salt, async (err, hash) => {
+  //     if (err) return next(new errors.InternalError(err.message));
+  //     user.password = hash;
+  //     try {
+  //       // Save user
+  //       const newUser = await user.save();
+  //       res.send(201, { newUser });
+  //       next();
+  //     } catch (err) {
+  //       return next(new errors.ConflictError(err.message)); // 409 status
+  //     }
+  //});
+  // });
+};
+
+/**
+ *  User Login function
+ *  It will check the password in auth.authenticate using bcryptjs,
+ *  If success , it will create and return JWT to client
+ * @returns iat,exp,token
+ * @throws UnauthorizedError 401
+ */
+exports.user_login = async (ctx, next) => {
+  const { email, password } = ctx.request.body;
+  try {
+    const user = await auth.authenticate(email, password);
+
+    // Create JWT and send to the client
+    const token = jwt.sign(user.toJSON(), config.JWT_SECRET, {
+      expiresIn: "1d"
+    });
+    const { iat, exp } = jwt.decode(token);
+    ctx.status = 200;
+    ctx.body = {
+      iat,
+      exp,
+      token,
+      code: 0
+    };
+  } catch (err) {
+    ctx.status = 401;
+    ctx.body = { err, code: -1 };
+  }
+};
--- Server/api/models/User.js
+++ Server/api/models/User.js
@@ -5,3 +5,35 @@
  * @since: Monday, 30th March 2020 10:27:42 pm
  * -----
  */
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  avatar: {
+    type: String
+  },
+  userType: {
+    type: Number, // 0: admin 1:user
+    default: 1
+  }
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
